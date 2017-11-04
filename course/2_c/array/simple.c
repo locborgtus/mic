dda9b00dc709aee8a939b639fc407d4f80f2fd59
Simon Wong <simon.wong@w2-research.com>
2017-11-03 23:05:48 -0700

    Added 2D array sample

--- course/2_c/array/simple.c
+++ course/2_c/array/simple.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
 
+const int size = 10;
+
+void print(int array[])
+{
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", array[i]);
+  }
+}
+
+void print2d(int array2d[][10])
+{
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf("%d\n", array2d[i][j]);
+    }
+  }
+}
+
 int main()
 {
-  int size = 10;
   int array[size];
+  int array2d[size][size];
+  int a2[11];
 
   for (int i = 0; i < size; i++) {
     array[i] = i + 100;
   }
 
   for (int i = 0; i < size; i++) {
+    a2[i + 1] = array[i];
+  }
+
+  print(array);
+
+  for (int i = 0; i < size; i++) {
     printf("%d\n", array[i]);
+  }
+
+  for (int i = 0; i < 11; i++) {
+    printf("%d\n", a2[i]);
   }
 }
